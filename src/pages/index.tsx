6e8473f215648b3720d8fd29053c687d312c00ca
Lucas Fernandes <lucas_matheus22@hotmail.com>
2022-12-30 10:59:29 -0300

    Estrutura da Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,30 @@
+import Image from 'next/image';
+
+import camisa1 from '../assets/camisas/1.png';
+import camisa2 from '../assets/camisas/2.png';
+import camisa3 from '../assets/camisas/3.png';
+import { HomeContainer, Product } from '../styles/pages/home';
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <HomeContainer>
+      <Product>
+        <Image src={camisa1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camisa2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  );
 }
