829309bfdd97f0dcfa9c9cb7586412375b362dea
Lucas Fernandes <lucas_matheus22@hotmail.com>
2023-01-03 10:23:23 -0300

    Estrutura da página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,10 +1,27 @@
-import { useRouter } from "next/router";
-import React from "react";
+import { useRouter } from 'next/router';
+import React from 'react';
+
+import { ImageContainer, ProductContainer, ProductDetail } from '../../styles/pages/product';
 
 const Product = () => {
   const { query } = useRouter();
 
-  return <h1>Product: {JSON.stringify(query)}</h1>;
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+      <ProductDetail>
+        <h1>Camisa X</h1>
+        <span>R$ 79,90</span>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. At officiis
+          porro tempore ipsum nobis aspernatur voluptatem error inventore
+          corporis quam vitae rem reiciendis, explicabo facere debitis natus
+          iusto pariatur. Rerum.
+        </p>
+        <button>Comprar agora</button>
+      </ProductDetail>
+    </ProductContainer>
+  );
 };
 
 export default Product;
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,6 +4,7 @@ export const globalStyles = globalCss({
   "*": {
     margin: 0,
     padding: 0,
+    boxsizing: "border-box",
   },
 
   body: {
